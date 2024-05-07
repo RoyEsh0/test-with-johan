5f6a333b9802a843a0a3e8c32e13391afe35938d
Nadir <nadirali342@gmail.com>
2024-05-07 14:39:51 +0200

    Fixade testet och körde den igen -> success

--- tests/stack-v1.test.js
+++ tests/stack-v1.test.js
@@ -1,10 +1,10 @@
 const stack = require('../src/stack');
 
 test('pop on stack with one element returns that element', () => {
-    stack.push("");
-    expect(stack.pop()).toBe("");
+    stack.push(2);
+    expect(stack.pop()).toBe(2);
 });
 
 test('pop on empty stack returns undefined', () => {
-    expect(stack.pop()).toBe(defined);    
+    expect(stack.pop()).toBe(undefined);    
 });
